476efd11d87c0fb98ee4642fe2cb57bd38983bce
James Willhoite <jameswillhoite@gmail.com>
2019-01-17 22:21:20 -0500

    still working on getting Jax-Rs to reply

--- src/main/java/com/openmtrapi/api/HelloWorldTest.java
+++ src/main/java/com/openmtrapi/api/HelloWorldTest.java
@@ -3,14 +3,16 @@ package com.openmtrapi.api;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloWorldTest extends Application {
     @GET
-    @Path("/{param}")
-    @Produces("application/json")
-    public Response getMsg(@PathParam("param") String msg) {
+    @Path("/{name}")
+    @Produces("text/plain")
+    public Response getMsg(@PathParam("name") String msg) {
         String output = "Jersey say : " + msg;
         return Response.status(200).entity(output).build();
     }
