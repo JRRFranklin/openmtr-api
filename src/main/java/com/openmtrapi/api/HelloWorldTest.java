7119c2b5acfd564e13acfe06b2f2341a54c1184c
James Willhoite <jameswillhoite@gmail.com>
2019-01-20 11:35:56 -0500

    Cannot get Jax to respond

--- src/main/java/com/openmtrapi/api/HelloWorldTest.java
+++ src/main/java/com/openmtrapi/api/HelloWorldTest.java
@@ -1,19 +1,20 @@
 package com.openmtrapi.api;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Path;
 
-@Path("/hello")
-public class HelloWorldTest extends Application {
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class HelloWorldTest {
+
+    // The Java method will process HTTP GET requests
     @GET
-    @Path("/{name}")
+    // The Java method will produce content identified by the MIME Media
+    // type "text/plain"
     @Produces("text/plain")
-    public Response getMsg(@PathParam("name") String msg) {
-        String output = "Jersey say : " + msg;
-        return Response.status(200).entity(output).build();
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello World";
     }
 }
